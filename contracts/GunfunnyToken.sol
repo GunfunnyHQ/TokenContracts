874e5f0a3dd8b92bd940bad82f35737cd26273b7
GunfunnyHQ <102752578+GunfunnyHQ@users.noreply.github.com>
2022-04-09 15:04:52 +0700

    Fixed some security bugs.

--- contracts/GunfunnyToken.sol
+++ contracts/GunfunnyToken.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.2;
+pragma solidity 0.8.4;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
@@ -42,17 +42,6 @@ abstract contract TwoPhaseOwnable is Context {
     }
 
     /**
-     * @dev Leaves the contract without owner. It will not be possible to call
-     * `onlyOwner` functions anymore. Can only be called by the current owner.
-     *
-     * NOTE: Renouncing ownership will leave the contract without an owner,
-     * thereby removing any functionality that is only available to the owner.
-     */
-    function renounceOwnership() public virtual onlyOwner {
-        _setOwner(address(0));
-    }
-
-    /**
      * @dev Transfers ownership of the contract to a new account (`newOwner`).
      * Can only be called by the current owner.
      */
@@ -79,10 +68,12 @@ interface IBPContract {
 
 contract GunfunnyToken is ERC20, ERC20Burnable, Pausable, TwoPhaseOwnable {
 
-    IBPContract public bpContract;
-
-    bool public bpEnabled;
-    bool public bpDisabledForever;
+	uint256 public startTime;
+	uint256 public endTime;
+	uint256 public maxAmount;
+	address public LPAddress;
+    mapping (address => bool) listingBlacklisted;
+    mapping (address => uint256) lastBuy;
 
     constructor() ERC20("Gunfunny Token", "GFY") {
         _mint(msg.sender, 5000000000 * (10 ** uint256(decimals())));  
@@ -96,40 +87,39 @@ contract GunfunnyToken is ERC20, ERC20Burnable, Pausable, TwoPhaseOwnable {
         _unpause();
     }
 
-
-    function setBPContract(address addr)
-        public
-        onlyOwner
-    {
-        require(addr != address(0), "BP adress cannot be 0x0");
-
-        bpContract = IBPContract(addr);
+    function checkListingBlacklisted(address _user) view external returns(bool) {
+        return listingBlacklisted[_user];
     }
 
-    function setBPEnabled(bool enabled)
-        public
-        onlyOwner
-    {
-        bpEnabled = enabled;
+    function setupListing(address _LPAddress, uint256 _maxAmount, uint256 _startTime, uint256 _endTime) external onlyOwner {
+		LPAddress = _LPAddress;
+		maxAmount = _maxAmount;
+		startTime = _startTime;
+		endTime = _endTime;
+	}
+
+    function blackListListing(address _evil, bool _black) external onlyOwner {
+        listingBlacklisted[_evil] = _black;
     }
 
-    function setBPDisableForever()
-        public
-        onlyOwner
-    {
-        require(!bpDisabledForever, "Bot protection disabled");
-
-        bpDisabledForever = true;
-    }
-
-    function _beforeTokenTransfer(address from, address to, uint256 amount)
-        internal
-        whenNotPaused
-        override
-    {
-        if (bpEnabled && !bpDisabledForever) {
-            bpContract.protect(from, to, amount);
+    function batchBlackListListing(address[] memory _evil, bool[] memory _black) external onlyOwner {
+        for (uint256 i = 0; i < _evil.length; i++) {
+            listingBlacklisted[_evil[i]] = _black[i];
         }
-        super._beforeTokenTransfer(from, to, amount);
     }
+
+	function transfer(address recipient, uint256 amount) public virtual override returns (bool) {
+		if (msg.sender == LPAddress) {
+            require(block.timestamp >= startTime, "GFY: Not yet open for trading");
+            require(!listingBlacklisted[recipient], "GFY: This recipient was blacklisted!");
+            if (block.timestamp < endTime) {
+			    require(amount <= maxAmount, 'GFY: maxAmount exceed listing!');
+                require(lastBuy[recipient] != block.number, "GFY: You already purchased in this block!");
+                lastBuy[recipient] = block.number;
+            }
+		}
+		_transfer(_msgSender(), recipient, amount);
+		return true;
+	}
+
 }
